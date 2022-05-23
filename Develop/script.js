c9ab85296fb770eeb2efa4a4285f79bb2af9c827
fumixer <fumixer@gmail.com>
2022-05-23 08:29:45 -0400

    PasswordLogic update

--- Develop/script.js
+++ Develop/script.js
@@ -1,67 +1,70 @@
-
-
-
 const passwordListElement = document.getElementById('generate')
 
-//Start the process after the clik of "Generate Password". Deactivate the reloaing page because no data is created here.
-
+// Start the process after the clik of "Generate Password". Deactivate the reloaing page because no data is created here.
 document.getElementById("generate").addEventListener("click", event => {
   event.preventDefault();
 
-//confirm the numerica characters
-var generateBtn = window.confirm("Click OK to confirm including numeric characters")
-// If the answer was smaller than 8, confirm the text //
- var generateBtn = window.alert("Password length must be at least 8 characters")
-// Ask including special characters
-  var generateBtn = window.confirm('Click OK to confirm including special characters.');
-//Ask how many caracters they want
-  var passwordLength = window.prompt("How many characters would  you like your password to contain?")
-//confirm the numerica characters
+  //confirm the numerica characters
   var generateBtn = window.confirm("Click OK to confirm including numeric characters")
-//confrim lowercase characters
+  // If the answer was smaller than 8, confirm the text //
+  var generateBtn = window.alert("Password length must be at least 8 characters")
+  // Ask including special characters
+  var generateBtn = window.confirm('Click OK to confirm including special characters.');
+  //Ask how many caracters they want
+  var passwordLength = window.prompt("How many characters would  you like your password to contain?")
+  //confirm the numerica characters
+  var generateBtn = window.confirm("Click OK to confirm including numeric characters")
+  //confrim lowercase characters
   var generateBtn = window.confirm("Click OK to confirm including lowercase characters.")
 
   //Stop duplicated/multiple expression of password
-    passwordListElement.innerHTML = '';
+  passwordListElement.innerHTML = '';
 
-  // Sending the result to HTML
-const li = document.createElement('li'),
+  //Sending the result to HTML
+  const li = document.createElement('li'),
     input = document.createElement('input');
-    input.value = passwords;
-    li. appendChild(input);
-    passwordListElement.appendChild(li);
-  })
-
+  input.value = passwords;
+  li.appendChild(input);
+  passwordListElement.appendChild(li);
+})
 
 //Password creation logic
-  // making lists of texts and number
+// making lists of texts and number
 const passwordTexts = [];
-  // making lists of small alphabets
+// making lists of small alphabets
 for (let i = 0; i < 26; i++) {
   passwordTexts.push((String.fromCharCode(('a'.charCodeAt(0)) + i)))
 }
-  //making lists of capital aphabets
+//making lists of capital aphabets
 for (let i = 0; i < 26; i++) {
   passwordTexts.push((String.fromCharCode(('A'.charCodeAt(0)) + i)))
 }
-  // making lists of numbers
+// making lists of numbers
 for (let i = 0; i < 10; i++) {
   passwordTexts.push(i)
-  //making lists of special caracters
-  //for (let i = "!\"#$%&'()-=^~\\|@`[{;+:*]},<.>/?\_" ,) {
-  //passwordTexts.push(i)
+}
+//making lists of special caracters
+for (let i = 0; i < 20; i++) {
+    passwordTexts.push((String.fromCharCode(('!'.charCodeAt(0)) + i)))
 }
 console.log(passwordTexts)
 
- //conditions
-//const passwordListElement = document.getElementById('generate'),
-      const passwordLength = 8;
-      const generateNumberOfTimes = 1;
+//Password creatin logic - 2
+// let letters = 'abcdefghijklmnopqrstuvwxyz';
+// let numbers = '0123456789';
+// let symbols = '!#$%&@`+:*?][()'
+// let password = letters + letters.toUpperCase() + numbers + symbols;
+// console.log(password)
 
-// // //Pasword length
+//conditions
+//const passwordListElement = document.getElementById('generate'),
+const passwordLength = 8;
+const generateNumberOfTimes = 1;
+
+// Pasword length
 // let passwordLength =(value);
 
-  //Generate random number
+//   //Generate random number
 let passwords = '';
 for (let i = 0; i < passwordLength; i++) {
   passwords += passwordTexts[Math.floor(Math.random() * passwordTexts.length)];
@@ -69,23 +72,7 @@ for (let i = 0; i < passwordLength; i++) {
 console.log(passwords)
 document.getElementById
 
-
-///////////////////////////////////////
-// generateBtn.addEventListener("click", writePassword);
-
-// function writePassword(){
-//   window.confirm("Click OK to confirm including special characters.");
-// }
-
-// Generating password//
-
-//if yes, 
-//else
-
-//hold the number
-
-///////////////////////////////////////
-
+////////////////////////////////////////
 
 // Write password to the #password input
 // function writePassword() {
